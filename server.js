069d81969ec355495d7fd2a4a338ce0e3748ff76
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2023-05-31 21:27:11 +0100

    added find product  by id

--- server.js
+++ server.js
@@ -1,28 +1,51 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require('./models/productModel')
+const Product = require("./models/productModel");
 
 const app = express();
 
-
-app.use(express.json())
+app.use(express.json());
 
 //routes
 app.get("/", (req, res) => {
   res.send("This is a test");
 });
 
-app.post('/product', async (req, res) => {
+app.get("/products", async (req, res) => {
   try {
-    const addedProduct = await Product.create(req.body)
-    res.status(200).json(addedProduct)
-
+    const fetchedProducts = await Product.find({});
+    handleSuccess(res, 200, fetchedProducts);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({message: error.message})
+    handleError(res, 500, error);
   }
+});
 
-})
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findProduct = await Product.findById(id);
+    handleSuccess(res, 200, findProduct);
+  } catch (error) {
+    handleError(res, 500, error);
+  }
+});
+app.post("/products", async (req, res) => {
+  try {
+    const addProducts = await Product.create(req.body);
+    handleSuccess(res, 200, addProducts);
+  } catch (error) {
+    handleError(res, 500, error);
+  }
+});
+
+//handle success
+function handleSuccess(res, statusCode, product) {
+  res.status(statusCode).json({ product });
+}
+//handle error
+function handleError(res, statusCode, error) {
+  res.status(statusCode).json({ message: error.message });
+}
 mongoose
   .connect(
     "mongodb+srv://admin:mohamednasir@cluster0.ld7yk.mongodb.net/Node-Project?retryWrites=true&w=majority"
