57449f82f12a256d0bcaabe9865c91cadc1e12e8
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2023-05-31 20:45:28 +0100

    added database connection & can add products to the database

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require('./models/productModel')
 
 const app = express();
+
+
+app.use(express.json())
 
 //routes
 app.get("/", (req, res) => {
   res.send("This is a test");
 });
 
+app.post('/product', async (req, res) => {
+  try {
+    const addedProduct = await Product.create(req.body)
+    res.status(200).json(addedProduct)
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({message: error.message})
+  }
+
+})
 mongoose
   .connect(
     "mongodb+srv://admin:mohamednasir@cluster0.ld7yk.mongodb.net/Node-Project?retryWrites=true&w=majority"
