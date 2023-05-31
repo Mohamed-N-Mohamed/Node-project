8f549af274e84d88d6e0e7d4ec53d281013edbbc
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2023-05-31 20:20:25 +0100

    added connection to mongodb

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
 
-
+const app = express();
 
 //routes
-app.get('/', (req, res) => {
-  res.send('Helo world')
-})
+app.get("/", (req, res) => {
+  res.send("This is a test");
+});
 
-app.listen(3000, () => {
-  console.log('server is running on port 3000')
-})
+mongoose
+  .connect(
+    "mongodb+srv://admin:mohamednasir@cluster0.ld7yk.mongodb.net/Node-Project?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to database");
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
+    });
+  })
+  .catch(() => console.log("Erorr something went wrong"));
